3927663e500873985a89976c280c5d2a4bfca661
Jonathan Roche <jonathan.roche@aerlingus.com>
2019-01-24 13:41:14 +0000

    Initial Commit

---
+++ src/main/java/org/rchtech/jwt/user/ApplicationUserRepository.java
@@ -0,0 +1,9 @@
+package org.rchtech.jwt.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ApplicationUserRepository extends JpaRepository<ApplicationUser, Long> {
+
+	ApplicationUser findByUsername(String username);
+
+}
